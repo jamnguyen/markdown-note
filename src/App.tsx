cb5e545ea2ab535245b7f1ca730cfb2c4350e25f
Jam Nguyen <jam@jamnguyen.com>
2025-06-15 11:26:18 +0700

    feat: setup MUI and remove CSS files

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
 import { useState } from 'react';
+import { Container, Typography, Button, Box, CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className='App'>
-      <h1>Electron + React + Vite</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth='sm'>
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}>
+          <Typography variant='h2' component='h1' gutterBottom>
+            Markdown Note
+          </Typography>
+          <Box sx={{ mt: 2 }}>
+            <Button variant='contained' onClick={() => setCount((count) => count + 1)}>
+              count is {count}
+            </Button>
+          </Box>
+          <Typography variant='body1' sx={{ mt: 2 }}>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </Typography>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
 
