c96e6379c9095b5e8f84a0323fe1d5589e71a6e9
Jam Nguyen <jam@jamnguyen.com>
2025-06-15 10:59:00 +0700

    feat: initialize Electron app with Vite and React, upgrade all packages to latest versions

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className='App'>
+      <h1>Electron + React + Vite</h1>
+      <div className='card'>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import electron from 'vite-plugin-electron';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    electron({
+      entry: 'electron/main.ts',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
